02fa22c12eac0340b39ef65f80b94b019a5a8bc0
Joshua <hillsjoshua7@gmail.com>
2023-03-17 08:59:29 +0000

    Betty is useless

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-gcc puts="/"Programming is like building a multilingual puzzle\n"
+
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 0 
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
