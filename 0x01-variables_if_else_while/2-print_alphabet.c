54e89f57b4aafa50f42111893259b37e5d0e359c
Joshua <hillsjoshua7@gmail.com>
2023-03-18 11:18:09 -0700

    Print Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - writes alphabet letters
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
