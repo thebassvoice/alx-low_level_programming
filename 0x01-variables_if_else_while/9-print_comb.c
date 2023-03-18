118dc9900697f64fda39c70da37fe37847bac478
Joshua <hillsjoshua7@gmail.com>
2023-03-18 14:24:42 -0700

    combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,16 @@ int main(void)
 {
 	int j;
 
-	for (j = 0 ; j <= 9 ; j++)
+	for (j = 0 ; j < 10 ; j++)
 	{
-		putchar(j + '0');
-		putchar(',');
-		putchar(' ');
+		if (j == 9)
+			putchar(j + '0');
+		else
+		{
+			putchar(j + '0');
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar('\n');
 	return (0);
 }
