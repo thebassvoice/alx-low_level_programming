15ee720cbb7adc2334b2caf5358e6a6198350959
Joshua <hillsjoshua7@gmail.com>
2023-03-18 12:59:22 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints base 10 numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int e;
+
+	for (e = 0; e <= 9; e++)
+	putchar(e);
+	putchar('\n');
+	return (0);
+}
