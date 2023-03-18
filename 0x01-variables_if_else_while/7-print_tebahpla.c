8e028efef7cc4d870cc61d2b0f1d8c94bf0a1c65
Joshua <hillsjoshua7@gmail.com>
2023-03-18 13:28:31 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Printd reverse numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char j;
+
+	for (j = 'z' ; j >= 'a' ; j--)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
+
