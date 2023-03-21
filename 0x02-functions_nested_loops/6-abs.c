9f27086ef1e9d9b7ff28b002a7edd053593f931a
Joshua <hillsjoshua7@gmail.com>
2023-03-21 20:57:15 +0000

    I dont know what i am doing

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - Computes integer value
+ *
+ * @int: Parameter
+ * Return: always 0
+ */
+int _abs(int j)
+{
+	if (j >= 0)
+	{
+		return (j);
+	}
+	else
+	{
+		return (j * -1);
+	}
+}
