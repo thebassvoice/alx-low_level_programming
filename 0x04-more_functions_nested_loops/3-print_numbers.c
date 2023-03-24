bd93cf17b12a171aeb238e8d94a7347efd765781
Joshua <hillsjoshua7@gmail.com>
2023-03-24 03:17:16 -0700

    we keep trying

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers -Prints numbers
+ *
+ * Return: always 0
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0' ; c <= '9' ; c++)
+		_putchar(c);
+	_putchar('\n');
+}
