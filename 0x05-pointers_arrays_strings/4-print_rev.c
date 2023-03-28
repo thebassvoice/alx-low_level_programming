3ce4756e79c430270ff5e71a9b2d0fd4c876a560
Joshua <hillsjoshua7@gmail.com>
2023-03-28 05:39:09 -0700

    Reverse printing

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,9 +10,9 @@ void print_rev(char *s)
 	int emeldah;
 	int count = 0;
 
-	for (emeldah = 0; s[emeldah] != '\0'; emeldah++)
+	for (emeldah = 0 ; s[emeldah] != '\0' ; emeldah++)
 		count++;
-	for (emeldah = count; emeldah >= 0; emeldah--)
+	for (emeldah = count ; emeldah >= 0 ; emeldah--)
 		_putchar(s[emeldah]);
 	_putchar('\n');
 }
