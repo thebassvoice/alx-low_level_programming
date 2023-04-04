8c242a2ebb91c509f020e11621a99c0b5ff01275
Joshua <hillsjoshua7@gmail.com>
2023-04-04 15:14:24 -0700

    Wow

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int josh= 0;
+
+	for (; s[josh] >= '\0'; josh++)
+	{
+		if (s[josh] == c)
+			return (&s[josh]);
+	}
+	return (0);
+}
+
