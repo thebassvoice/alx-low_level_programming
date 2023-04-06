1d13b803e010b080bc9a02d1f3be5ed16cf599ea
Joshua <hillsjoshua7@gmail.com>
2023-04-06 01:31:18 -0700

    Marathon

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * wildcmp - main funtion
+ * @s1: parameter
+ * @s2: parameter
+ * Return: 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
+
