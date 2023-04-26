14b744c821e71d1980d3f1c764422f9bc38b996f
thebassvoice <hillsjoshua7@gmail.com>
2023-04-26 04:05:32 -0700

    Back at it

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * 
+ * Return: Always 0
+ */
+int main(void)
+{
+	int e;
+	unsigned long int l, c, after, sum;
+
+	l = 1;
+	c = 2;
+	sum = 0;
+	for (e = 1; e <= 33; ++e)
+	{
+		if (l < 4000000 && (l % 2) == 0)
+		{
+			sum = sum + l;
+		}
+		after = l + c;
+		l = c;
+		c = after;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
