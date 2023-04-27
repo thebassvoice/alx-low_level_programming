6c9dec6f39bb9441e49ca46c03ae66dbfe42197a
thebassvoice <hillsjoshua7@gmail.com>
2023-04-27 03:12:29 -0700

    Add

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * add_node - main function
+ * @head: pointer to a pointer
+ * @str: pointer
+ * Return: pointer to a pointer
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *i;
+	size_t character;
+
+	i = malloc(sizeof(list_t));
+	if (i == NULL)
+		return (NULL);
+	i->str = strdup(str);
+	for (character = 0; str[character]; character++);
+	i->len = character;
+	i->next = *head;
+	*head = i;
+
+	return (*head);
+}
