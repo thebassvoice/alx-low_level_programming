f1877e12eecd30b8a4cd7a17ddbf3eb96529d2f5
thebassvoice <hillsjoshua7@gmail.com>
2023-05-01 03:35:49 -0700

    More lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_listint2 - main linked list free
+ * @head: pointer parameter
+ *
+ * Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+	listint_t *anotherPtr;
+
+	if (head != NULL)
+	{
+		anotherPtr = *head;
+		while ((ptr = anotherPtr) != NULL)
+		{
+			anotherPtr =anotherPtr->next;
+		free(ptr);
+		}
+	*head = NULL;
+	}
+}
