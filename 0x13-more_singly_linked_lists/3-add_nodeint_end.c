4fd5d7571c21972cdd412f457dd7a6afacdd4230
thebassvoice <hillsjoshua7@gmail.com>
2023-05-01 02:49:42 -0700

    idk

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,20 +9,30 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *latest;
-	listint_t *ptr = *head;
+	listint_t *ptr;
+
+	(void)ptr;
 
 	latest = malloc(sizeof(listint_t));
-	if (!latest)
+
+	if (latest == NULL)
 		return (NULL);
+
 	latest->n = n;
 	latest->next = NULL;
+	ptr = *head;
 	if (*head == NULL)
 	{
 		*head = latest;
-		return (latest);
 	}
-	while (ptr->next)
-		ptr = ptr->next;
-	ptr->next = latest;
-	return (latest);
+	else
+	{
+		while (ptr->next != NULL)
+		{
+			ptr = ptr->next;
+		}
+		ptr->next = latest;
+	}
+
+	return (*head);
 }
