eab78cab46e7016f8a052d9d4db36853a810ce1c
thebassvoice <hillsjoshua7@gmail.com>
2023-05-04 08:09:26 -0700

    At it again

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_binary - main function
+ * @n: parameter
+ *
+ * Return: Nothing
+ */
+void print_binary(unsigned long int n)
+{
+	int bin, j;
+	unsigned long int latest;
+
+	j = 0;
+
+	for (bin = 63; bin >= 0; bin--)
+	{
+		latest = n >> bin;
+		if (latest & 1)
+		{
+			_putchar('1');
+			j++;
+		}
+		else if (j)
+			_putchar('0');
+	}
+	if (!j)
+		_putchar('0');
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 unsigned int binary_to_uint(const char *b);
-
+void print_binary(unsigned long int n);
 
 
 
